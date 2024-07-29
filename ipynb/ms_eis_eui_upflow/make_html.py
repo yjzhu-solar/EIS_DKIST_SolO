4909c037ce6c53cf967154e294eba3ec7174b202
yjzhu-solar <yingjie.zhu@pmodwrc.ch>
2024-07-29 11:22:18 +0200

    add a plot to show the halloween smile face

--- ipynb/ms_eis_eui_upflow/make_html.py
+++ ipynb/ms_eis_eui_upflow/make_html.py
@@ -10,19 +10,19 @@ output_dir = '../../eis_eui_upflow_ipynb_html/'
 if os.path.exists('latest_mod_date.csv'):
     latest_mod_date = pd.read_csv('latest_mod_date.csv')
 
+    file_name_new = []
+    mod_date_new = []
+    file_name_updated = []
+    mod_date_updated = []
+    
     for file in files:
-        file_name_new = []
-        mod_date_new = []
-        file_name_updated = []
-        mod_date_updated = []
         if file not in latest_mod_date['file_name'].values:
             file_name_new.append(file)
             mod_date_new.append(os.path.getmtime(file))
-            
             ipy2html(file, output_dir)
         else:
             index = latest_mod_date[latest_mod_date['file_name'] == file].index[0]
-            if os.path.getmtime(file) > latest_mod_date.loc[index, 'mod_date']:
+            if os.path.getmtime(file) - latest_mod_date.loc[index, 'mod_date'] > 0.2:
                 ipy2html(file, output_dir)
                 latest_mod_date.loc[index, 'mod_date'] = os.path.getmtime(file)
                 file_name_updated.append(file)
